aac5f70855bde5d36451ada982f3c54fd1c20d2b
alphasr <nilaybaranwal@gmail.com>
2019-11-11 00:13:54 +0100

    fixed content image but still not displaying

--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -1,23 +1,42 @@
 package  main
+import java.nio.file.{Files, Paths}
 import java.net._
 import java.io._
+
+import scala.concurrent.TimeoutException
 import scala.io._
 
 object server extends App {
-val http = "HTTP/1.1 200 OK\n"
+val http200 = "HTTP/1.1 200 OK\n"
 val content = "Content-Type: text/html\n"+"Content-Length: "
+val contentimage = "Content-Type: image/"
 
   //send.println("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 50\n\n<html><body><h1> YARRAMI YE ! </h1></body></html>")
 
   class request(socket: ServerSocket) {
-    def create():Unit={
-      while(true){
+    def create():Unit= {
+      while (true) {
         val Client = socket.accept()
-        val thread = new Thread(new Runnable () {
-          def run():Unit = serve(Client)
-        })
-        thread.start()
+        try {
+          val thread = new Thread(new Runnable() {
+            def run(): Unit = serve(Client)
+          })
+          thread.start()
+        }
+
+        catch {
+          case e: TimeoutException => err_500() // error 500
+        }
+
       }
+    }
+    //ERROR FUNCTIONS
+    def error_405():String= {
+      val input = Source.fromFile("./src/err/405.html")
+      val ss = input.getLines().mkString
+      var re =""
+      re+="HTTP/1.1 405 Not Found\n"+content + ss.length + "\n\n" +ss
+      re
     }
 
     //ERROR FUNCTIONS
@@ -29,7 +48,10 @@ val content = "Content-Type: text/html\n"+"Content-Length: "
       re
     }
 
-    def err_500() = ??? /// TODO AZIMJON
+    def err_500():String={
+      Runtime.getRuntime.addShutdownHook( new Thread( () => server.stop() ))
+      "this"
+    }
 
     //ERROR FUNCTIONS
 
@@ -41,7 +63,7 @@ val content = "Content-Type: text/html\n"+"Content-Length: "
        val ss = input.getLines().mkString
        var re=""
        //send.println("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 50\n\n<html><body><h1> YARRAMI YE ! </h1></body></html>")
-       re += http + content +  ss.length + "\n\n" + ss
+       re += http200 + content +  ss.length + "\n\n" + ss
        re
      }
 
@@ -53,21 +75,57 @@ val content = "Content-Type: text/html\n"+"Content-Length: "
     }
 
 
+    def sendImage(filename: String, extension:String,send:PrintWriter,os:OutputStream): Unit ={
+
+
+      val location = "./src/static" + filename
+
+      try {
+        val byteArray = Files.readAllBytes(Paths.get(location))
+        var re=""
+        re+= http200 + contentimage+extension+ "\nContent-Length: "+byteArray.length + "\n\n"
+        println(re)
+        send.println(re)
+        send.flush()
+        os.write(byteArray,0,byteArray.length)
+        os.flush()
+      }
+      catch {
+        case e: FileNotFoundException => error_404()
+        case e: IOException => err_500()
+      }
+    }
+
 
     def serve(socket: Socket): Unit = {
       val reader = new BufferedReader(new InputStreamReader(socket.getInputStream))
       val getreq = reader.readLine()
       val s = getreq.split(" ")
       val send = new PrintWriter( new BufferedWriter( new OutputStreamWriter( socket.getOutputStream ) ) )
+      val os =socket.getOutputStream
+
       if(s(0) == "GET"){
         val a = s(1).split('.')
           val suffix = a(1)
+       // println(suffix)
         suffix match{
           case "html" => send.println(sendhtml(s(1)))
+          case "css" => send.println(sendhtml(s(1)))
+          case "css" => send.println(sendhtml(s(1)))
+          case "js" => send.println(sendhtml(s(1)))
+
+          case "png" => sendImage(s(1),"png",send,os)
+          case "jpeg" =>sendImage(s(1),"jpeg",send,os)
+          case "jpg" => sendImage(s(1),"jpg",send,os)
+          case "gif" => sendImage(s(1),"gif",send,os)
+          case "svg" => sendImage(s(1),"svg",send,os)
+          case _ =>
         }
       }
+      else send.println(error_405())
       //send.println("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 50\n\n<html><body><h1> YARRAMI YE ! </h1></body></html>")
       send.flush()
+      os.flush()
     }
     def stop(): Unit = {
       println("Stopped")
