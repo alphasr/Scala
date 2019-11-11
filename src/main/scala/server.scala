f4ff4cc3fd617dbb66ef03dac7783afd202dcc86
Egemen251 <egemen251@gmail.com>
2019-11-11 12:19:40 +0100

    added image proccessing , home page and some other error handling

--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -1,80 +1,32 @@
 package  main
-import java.nio.file.{Files, Paths}
 import java.net._
 import java.io._
+import java.nio.file.{Files, Paths}
 
-import scala.concurrent.TimeoutException
+import main.server.http200
+
 import scala.io._
 
 object server extends App {
-val http200 = "HTTP/1.1 200 OK\n"
-val content = "Content-Type: text/html\n"+"Content-Length: "
-val contentimage = "Content-Type: image/"
+  val http200 = "HTTP/1.1 200 OK\n"
+  val content = "Content-Type: text/html\n"+"Content-Length: "
+  val content2 =  "Content-Type: dir\n"+"Content-Length: "
+  val contentimage =  "Content-Type: image/"
 
   //send.println("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 50\n\n<html><body><h1> YARRAMI YE ! </h1></body></html>")
 
   class request(socket: ServerSocket) {
-    def create():Unit= {
-      while (true) {
+    def create():Unit={
+      while(true){
         val Client = socket.accept()
-        try {
-          val thread = new Thread(new Runnable() {
-            def run(): Unit = serve(Client)
-          })
-          thread.start()
-        }
-
-        catch {
-          case e: TimeoutException => err_500() // error 500
-        }
-
+        val thread = new Thread(new Runnable () {
+          def run():Unit = serve(Client)
+        })
+        thread.start()
       }
     }
-    //ERROR FUNCTIONS
-    def error_405():String= {
-      val input = Source.fromFile("./src/err/405.html")
-      val ss = input.getLines().mkString
-      var re =""
-      re+="HTTP/1.1 405 Not Found\n"+content + ss.length + "\n\n" +ss
-      re
-    }
 
-    //ERROR FUNCTIONS
-    def error_404():String= {
-      val input = Source.fromFile("./src/err/404.html")
-      val ss = input.getLines().mkString
-      var re =""
-      re+="HTTP/1.1 404 Not Found\n"+content + ss.length + "\n\n" +ss
-      re
-    }
-
-    def err_500():String={
-      Runtime.getRuntime.addShutdownHook( new Thread( () => server.stop() ))
-      "this"
-    }
-
-    //ERROR FUNCTIONS
-
-    def sendhtml(filename:String):String={
-
-      val location = "./src/static" + filename
-     try {
-       val input = Source.fromFile(location)
-       val ss = input.getLines().mkString
-       var re=""
-       //send.println("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 50\n\n<html><body><h1> YARRAMI YE ! </h1></body></html>")
-       re += http200 + content +  ss.length + "\n\n" + ss
-       re
-     }
-
-      catch {
-        case e: FileNotFoundException => error_404()
-        case e: IOException => err_500()
-      }
-
-    }
-
-
+    //IMAGE PROCCES
     def sendImage(filename: String, extension:String,send:PrintWriter,os:OutputStream): Unit ={
 
 
@@ -95,37 +47,130 @@ val contentimage = "Content-Type: image/"
         case e: IOException => err_500()
       }
     }
+    //IMAGE PROCCESS
+
+    //ERROR FUNCTIONS
+    def error_404():String= {
+      val input = Source.fromFile("./src/err/404.html")
+      val ss = input.getLines().mkString
+      var re =""
+      re+="HTTP/1.1 404 Not Found\n"+content + ss.length + "\n\n" +ss
+      re
+    }
+
+    def err_500(): String = {
+      val input = Source.fromFile("src/err/500.html")
+      val ss = input.getLines().mkString
+      var re =""
+      re+="HTTP/1.1 500 IO EXCEPTION\n"+content + ss.length + "\n\n" +ss
+      re
+    }
+
+
+    def error_403():String= {
+      val input = Source.fromFile("./src/err/403.html")
+      val ss = input.getLines().mkString
+      var re =""
+      re+="HTTP/1.1 Permission Denied\n"+content + ss.length + "\n\n" +ss
+      re
+    }
+
+    def error_405():String= {
+      val input = Source.fromFile("./src/err/405.html")
+      val ss = input.getLines().mkString
+      var re =""
+      re+="HTTP/1.1 M\n"+content + ss.length + "\n\n" +ss
+      re
+    }
+    //ERROR FUNcTIONS
+
+    def sendhtml(filename:String):String={
+      var location = ""
+      if(filename == "/src"){
+        val ss = "Directories: \n\nhome\n\nstatic\n\nmain"
+        var re = ""
+        //send.println("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 50\n\n<html><body><h1> YARRAMI YE ! </h1></body></html>")
+        re += http200+ content2 + ss.length + "\n\n" + ss
+        re
+      }
+
+      else {
+        if(filename contains ".html"){
+          location = "./src/" + filename
+        }
+        else {
+          location = "./src/" + filename + "/index.html"
+        }
+        try {
+          val input = Source.fromFile(location)
+          val ss = input.getLines().mkString
+          var re = ""
+          //send.println("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 50\n\n<html><body><h1> YARRAMI YE ! </h1></body></html>")
+          re += http200 + content + ss.length + "\n\n" + ss
+          re
+        }
+
+        catch {
+          case e: FileNotFoundException => error_404()
+          case e: IOException => err_500()
+        }
+      }
+    }
+
 
 
     def serve(socket: Socket): Unit = {
       val reader = new BufferedReader(new InputStreamReader(socket.getInputStream))
       val getreq = reader.readLine()
+      println(getreq)
       val s = getreq.split(" ")
       val send = new PrintWriter( new BufferedWriter( new OutputStreamWriter( socket.getOutputStream ) ) )
-      val os =socket.getOutputStream
+      val os =new DataOutputStream(socket.getOutputStream)
+      //println(s(1))
+      val check = s(1).split('.')
+      if(s(0) == "GET" ) {
+        if (getreq == "GET / HTTP/1.1") {
+          send.println(sendhtml("home/index.html") + getreq + 200 )
+        }
+        else if(s(1) == "/src"){
+          send.println(sendhtml(s(1)))
+        }
+        else if(check.length == 1){
+          //val a:String = check(0)
 
-      if(s(0) == "GET"){
-        val a = s(1).split('.')
+          val n = s(1).split('/')
+          if((n(1) contains "main") || (n(1) contains "classified") ){
+            send.println(error_403())
+          }
+          else {
+            //println("n:" + n(1))
+            send.println(sendhtml(n(1)))
+          }
+        }
+        else {
+          val a = s(1).split('.')
           val suffix = a(1)
-       // println(suffix)
-        suffix match{
-          case "html" => send.println(sendhtml(s(1)))
-          case "css" => send.println(sendhtml(s(1)))
-          case "css" => send.println(sendhtml(s(1)))
-          case "js" => send.println(sendhtml(s(1)))
+          suffix match {
+            case "html" => send.println(sendhtml(s(1)))
+            case "css" => send.println(sendhtml(s(1)))
+            case "css" => send.println(sendhtml(s(1)))
+            case "js" => send.println(sendhtml(s(1)))
 
-          case "png" => sendImage(s(1),"png",send,os)
-          case "jpeg" =>sendImage(s(1),"jpeg",send,os)
-          case "jpg" => sendImage(s(1),"jpg",send,os)
-          case "gif" => sendImage(s(1),"gif",send,os)
-          case "svg" => sendImage(s(1),"svg",send,os)
-          case _ =>
+            case "png" => sendImage(s(1),"png",send,os)
+            case "jpeg" =>sendImage(s(1),"jpeg",send,os)
+            case "jpg" => sendImage(s(1),"jpg",send,os)
+            case "gif" => sendImage(s(1),"gif",send,os)
+            case "svg" => sendImage(s(1),"svg",send,os)
+            case _ =>
+          }
         }
       }
-      else send.println(error_405())
+
+      else{
+        send.println(sendhtml("err/405.html"))
+      }
       //send.println("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 50\n\n<html><body><h1> YARRAMI YE ! </h1></body></html>")
       send.flush()
-      os.flush()
     }
     def stop(): Unit = {
       println("Stopped")
